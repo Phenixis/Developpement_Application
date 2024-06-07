8ed1751a86bf34891406d13f5f44e928026bbb8f
CyprienIUT <cpe.albert2@gmail.com>
2024-06-07 09:26:19 +0200

    update controleur

---
+++ SAE201/src/radioMonteCarlo/controller/main.java
@@ -0,0 +1,5 @@
+package radioMonteCarlo.controller;
+
+public class main {
+
+}
--- SAE201/src/radioMonteCarlo/view/CtrlSpectacle.java
+++ SAE201/src/radioMonteCarlo/view/CtrlSpectacle.java
@@ -1,5 +1,69 @@
 package radioMonteCarlo.view;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.ChoiceBox;
+import javafx.scene.control.ListView;
+import javafx.scene.control.TextField;
+import javafx.scene.image.ImageView;
+
 public class CtrlSpectacle {
+
+    @FXML private ImageView imageScene;
+
+    @FXML private Button bnDroite;
+
+    @FXML private TextField txtTarifBalcon;
+
+    @FXML private Button bnGauche;
+
+    @FXML private ListView<?> listViewArtistesSelectionnés;
+
+    @FXML
+    private Button bnValider;
+
+    @FXML
+    private TextField txtDuree;
+
+    @FXML
+    private TextField txtTarifLoges;
+
+    @FXML
+    private TextField txtNbSpec;
+
+    @FXML
+    private ListView<?> listViewArtistes;
+
+    @FXML
+    private TextField txtTarifOrchestre;
+
+    @FXML
+    private Button bnAnnuler;
+
+    @FXML
+    private TextField txtNom;
+
+    @FXML private ChoiceBox<?> cbGenre;
+
+    @FXML
+    void clicDroite(ActionEvent event) {
+
+    }
+
+    @FXML
+    void clicGauche(ActionEvent event) {
+
+    }
+
+    @FXML
+    void clicValider(ActionEvent event) {
+
+    }
+
+    @FXML
+    void clicAnnuler(ActionEvent event) {
+
+    }
 
 }
