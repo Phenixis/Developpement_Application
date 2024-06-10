ffe0783ff3372071a0664ff85f9052e105add9ec
Phenixis22 <max.duh22@gmail.com>
2024-06-10 14:07:54 +0200

    create Genre.contains

--- SAE201/src/radioMonteCarlo/model/Genre.java
+++ SAE201/src/radioMonteCarlo/model/Genre.java
@@ -5,6 +5,14 @@ import java.util.ArrayList;
 public class Genre {
 	
 	static String[] liste = {"Danse", "Opera", "Musique", "Cirque", "Marionnette", "Theatre"};
-
-
+	
+	static boolean contains(String value) {
+		for(String genre: Genre.liste) {
+			if (value.equals(genre)) {
+				return true
+			}
+		}
+		
+		return false;
+	}
 }
