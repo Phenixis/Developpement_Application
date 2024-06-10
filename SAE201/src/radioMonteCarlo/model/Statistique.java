82b8bb399f58ba9d6376775c9aade9a21a359c81
BalaiDuckman2 <raf29450@gmail.com>
2024-06-10 11:53:09 +0200

    Statistique

--- SAE201/src/radioMonteCarlo/model/Statistique.java
+++ SAE201/src/radioMonteCarlo/model/Statistique.java
@@ -3,15 +3,38 @@ package radioMonteCarlo.model;
 import java.util.ArrayList;
 
 public class Statistique {
+	Spectacle spectacle;
+	private String nomSpectacle;
+	private int nbRepresentation=0;
+	private int nbBillet=0;
+	private float moyBillet=0;
 	
-	
-	private Statistique() {
-		
+	private Statistique(Spectacle spectacle) {
+
+		this.nomSpectacle=spectacle.getNom();
+		this.nbRepresentation=spectacle.getListeRepresentations().size();
+		this.nbBillet=nombreBillet(spectacle);
+		this.moyBillet=moyenneBillet(spectacle);
 	}
 	
-	static Statistique calculerStats() {
-		
+	static Statistique calculerStats(Spectacle spec) {
+		return new Statistique(spec);
 	}
 
+	
+	private int nombreBillet(Spectacle spectacle){
+		int nb=0;
+		for(Representation i : spectacle.getListeRepresentations() ){
+			nb+=i.getNbBilletVendu();
+		}
+		return nb;
+	}	
+
+	private int moyenneBillet(Spectacle spectacle){
+		int nb=0;
+		nb=nbBillet/nbRepresentation;
+		return nb;
+	
+	}
 
 }
