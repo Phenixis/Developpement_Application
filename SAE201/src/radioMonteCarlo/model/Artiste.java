c9bb1ddd989e0565b9c3f9e6159bbb17aa8362b4
Phenixis22 <max.duh22@gmail.com>
2024-06-10 11:13:36 +0200

    artiste

--- SAE201/src/radioMonteCarlo/model/Artiste.java
+++ SAE201/src/radioMonteCarlo/model/Artiste.java
@@ -4,41 +4,53 @@ import java.util.ArrayList;
 
 public class Artiste {
 	String nom;
-	ArrayList<Spectacle> listeSpectacle=new ArrayList<>();
-	public Artiste(String nom, ArrayList<Spectacle> listeSpectacle) {
-		this.nom = nom;
-		this.listeSpectacle = listeSpectacle;
-	}
-	public Artiste(String nom) {
-		super();
-		this.nom = nom;
-	}
 	
+	// Relation avec Spectacle
+	ArrayList<Spectacle> listeSpectacle=new ArrayList<>();
+	
+	public Artiste(String nom) {
+		this.nom = nom;
+	}
 	
 	public String getNom() {
 		return nom;
 	}
+	
 	public void setNom(String nom) {
 		this.nom = nom;
 	}
-	public ArrayList<Spectacle> getListeSpectacle() {
-		return listeSpectacle;
-	}
-	public void ajouteSpectacle(Spectacle listeSpectacle) {
-		this.listeSpectacle.add(listeSpectacle);
+	
+	// Relation avec Spectacle
+	
+	public boolean contientSpectacle(Spectacle spectacle) {
+		return this.listeSpectacle.contains(spectacle);
 	}
 	
-	public void retirerSpectacle(Spectacle Spectacle) {
+	private void addSpectacle(Spectacle spectacle) {
+		this.listeSpectacle.add(spectacle);
+	}
+	
+	private void removeSpectacle(Spectacle spectacle) {
+		this.listeSpectacle.remove(spectacle);
+	}
+	
+	public void retirerSpectacle(Spectacle spectacle) {
 		if(this.listeSpectacle.size()!=1) {
-		    if (this.listeSpectacle.contains(Spectacle)) {
-		        this.listeSpectacle.remove(Spectacle);
+		    if (this.contientSpectacle(spectacle)) {
+		        this.removeSpectacle(spectacle);
 		    } else {
 		        System.out.println("Erreur : Spectacle spécifié n'est pas présent dans la liste.");
 		    }
-		}else {
-			System.out.println("Erreur : minimum 1 Spectacle");
+		} else {
+			System.out.println("Erreur : il faut au moins 1 spectacle");
 		}	
 	}
 	
-
+	public void ajouterSpectacle(Spectacle spectacle) {
+		if (this.contientSpectacle(spectacle)) {
+			System.out.println("Erreur : L'artiste joue déjà dans le spectacle.");
+		} else {
+			this.addSpectacle(spectacle);
+		}
+	}
 }
