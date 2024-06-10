4da475b68021022252646eaa70fb02ad8dada8c3
Phenixis22 <max.duh22@gmail.com>
2024-06-10 15:05:38 +0200

    improve heure

--- SAE201/src/radioMonteCarlo/model/Heure.java
+++ SAE201/src/radioMonteCarlo/model/Heure.java
@@ -1,8 +1,14 @@
 package radioMonteCarlo.model;
 
 public class Heure {
-	static public float intToMin(int heure) {
-		return heure/60;
+	static public float intToMin(int minutes) {
+		// minutes = 90, return = 1.5
+		return minutes/60;
+	}
+	
+	static public int minToInt(float heure) {
+		// heures = 1.5, return 90
+		return (int) (heure*90);
 	}
 	
 	static public String intToString(int heure) {
@@ -10,6 +16,11 @@ public class Heure {
 		int nbHeure = (int) (Heure.intToMin(heure)*60);
 		
 		return "" + nbHeure + ":" + (heure-nbHeure*60);
+	}
+	
+	static public int stringToInt(String heure) {
+		// heure = "01:30", return = 90
+		return Integer.parseInt(heure.substring(0, heure.indexOf(":")))*60 + Integer.parseInt(heure.substring(heure.indexOf(":")))
 	}
 	
 }
