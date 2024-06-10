3642b9d526f9cb70add61ffc5f04d955367f6f09
Phenixis22 <max.duh22@gmail.com>
2024-06-10 15:54:45 +0200

    accesseur pour Zone

--- SAE201/src/radioMonteCarlo/model/Zone.java
+++ SAE201/src/radioMonteCarlo/model/Zone.java
@@ -4,11 +4,15 @@ public class Zone {
 
     private String nom;
     
-
     public Zone(String nom) {
-        this.nom = nom;
-        
+        this.setNom(nom);
     }
-    
-    
+
+	public String getNom() {
+		return nom;
+	}
+
+	public void setNom(String nom) {
+		this.nom = nom;
+	}
 }
