6c0278452b9aa585e3233b8f1491f5032fd7e0a8
Phenixis22 <max.duh22@gmail.com>
2024-06-10 14:03:02 +0200

    create Genre

---
+++ SAE201/src/radioMonteCarlo/model/Genre.java
@@ -0,0 +1,10 @@
+package radioMonteCarlo.model;
+
+import java.util.ArrayList;
+
+public class Genre {
+	
+	static String[] liste = {"Danse", "Opera", "Musique", "Cirque", "Marionnette", "Theatre"};
+
+
+}
